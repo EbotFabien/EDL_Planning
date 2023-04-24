1b6d7905394a41707c8b002c7f5ac0ca6ea36eaf
Ebot Fabien <touchone0001@gmail.com>
2023-04-24 16:31:40 +0100

    done

---
+++ project/app/__init__.py
@@ -0,0 +1,45 @@
+
+from flask import Flask, render_template, url_for,flash,redirect,request, jsonify,make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_bcrypt import Bcrypt
+from flask_login import  LoginManager
+#from flask_mail import Mail
+from app.config import Config
+import os
+from firebase_admin import credentials, firestore, initialize_app
+from flask_cors import CORS
+# Initialize Flask App
+
+
+
+
+cred = credentials.Certificate('C:/EDL_Planning_app/project/app/key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+bcrypt = Bcrypt()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    with app.app_context():
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "http://127.0.0.1")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+
+   
+    bcrypt.init_app(app)
+    
+    
+    
+    from app.entity.User.route import user
+    from app.entity.RDV.route import rdv
+    from app.entity.Participant.route import participant
+    from app.entity.EDL.route import edl
+    
+    app.register_blueprint(user)
+    app.register_blueprint(rdv)
+    app.register_blueprint(participant)
+    app.register_blueprint(edl)
+    return app
